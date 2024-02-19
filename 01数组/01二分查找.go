a6e6cff985f2d400fe9a1c097ed829e9b9d261c7
gwsee <390227869@qq.com>
2024-02-19 11:17:38 +0800

    refactor:二分再次理解

--- 01数组/01二分查找.go
+++ 01数组/01二分查找.go
@@ -1,5 +1,14 @@
 package main
 
+/**
+这两种的区别是在于
+
+在对比后续轮次的时候的较大值(high),是否可能是目标(target)
+
+如果可能是的写法 就用 high = mid - 1 ; 然后 用low<=high
+如果不可能是的写法    high = mid;      然后 用low<high
+ */
+
 // 左闭右闭区间
 // 这种写法的实质是在于 在 arr[mid] > target 的时候  high最新的赋值是可能的target数；所以 才有low<=high
 func fun11(arr []int, target int) int {
@@ -8,12 +17,12 @@ func fun11(arr []int, target int) int {
 	}
 	high := len(arr) - 1
 	low := 0
-	for low <= high {
+	for low <= high { //不同点1
 		mid := low + (high-low)/2
 		if arr[mid] == target {
 			return mid
 		} else if arr[mid] > target {
-			high = mid - 1
+			high = mid - 1//不同点2
 		} else {
 			low = mid + 1
 		}
@@ -29,12 +38,12 @@ func fun12(arr []int, target int) int {
 	}
 	high := len(arr) - 1
 	low := 0
-	for low < high {
+	for low < high {//不同点1
 		mid := low + (high-low)/2
 		if arr[mid] == target {
 			return mid
 		} else if arr[mid] > target {
-			high = mid
+			high = mid//不同点2
 		} else {
 			low = mid + 1
 		}
