878380a2e20d16bb29e0c8e165b2177e25e4ba1c
gwsee <390227869@qq.com>
2024-03-04 16:02:04 +0800

    refactor:有效字母异位词

---
+++ 03哈希表/01有效的字母异位词.go
@@ -0,0 +1,56 @@
+package main
+
+import "fmt"
+
+/**
+
+给定两个字符串 s 和 t ，编写一个函数来判断 t 是否是 s 的字母异位词。
+
+示例 1: 输入: s = "anagram", t = "nagaram" 输出: true
+
+示例 2: 输入: s = "rat", t = "car" 输出: false
+
+说明: 你可以假设字符串只包含小写字母。
+
+ */
+
+func isAnagram(s string, t string) bool {
+	record := [26]int{}
+
+	for _, r := range s {
+		record[r-rune('a')]++
+	}
+	for _, r := range t {
+		record[r-rune('a')]--
+	}
+
+	return record == [26]int{}   // record数组如果有的元素不为零0，说明字符串s和t 一定是谁多了字符或者谁少了字符。
+}
+func isAnagram2(s string, t string) bool {
+	if len(s) != len(t) {
+		return false
+	}
+	records := [26]int{}
+	for index := 0; index < len(s); index++ {
+		if s[index] == t[index] {
+			continue
+		}
+		sCharIndex := s[index] - 'a'
+		records[sCharIndex]++
+		tCharIndex := t[index] - 'a'
+		records[tCharIndex]--
+	}
+	for _, record := range records {
+		if record != 0 {
+			return false
+		}
+	}
+	return true
+}
+func testF1()  {
+	fmt.Println(isAnagram("anagram","nagaram"))
+	fmt.Println(isAnagram("rat","car"))
+	fmt.Println("遍历方法2")
+	fmt.Println(isAnagram2("anagram","nagaram"))
+	fmt.Println(isAnagram2("rat","car"))
+}
